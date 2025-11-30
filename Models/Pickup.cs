a3d8a440160684a8ce1dee0940851562cb84a8f9
JaztinRojas <242494895+JaztinRojas@users.noreply.github.com>
2025-11-30 18:50:43 +0800

    pa check guys kung need ug improvements ;-;

---
+++ Models/Pickup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRENTAL.Models
+{
+    public abstract class Pickup : Vehicle
+    {
+        public decimal PayloadCapacity { get; set; }
+        public decimal TrunkCapacity { get; set; }
+
+        public abstract override decimal CalculateRate(decimal auto);
+    }
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRENTAL.Models
+{
+
+
+    public abstract class Vehicle
+{
+    public int VehicleId { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int Year { get; set; }
+    public string LicensePlate { get; set; }
+    public string ListType { get; set; }
+    public string VehicleStatus { get; set; }
+    public List<String> Photos { get; set; }
+    public decimal FuelCapacity { get; set; }
+
+    public abstract decimal CalculateRate(decimal auto);
+    public string CarStatus() => $"Vehicle {VehicleId}: {Model}";
+    public bool Availability() => VehicleStatus == "Avalibale";
+
+    }
+
+}
