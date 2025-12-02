ed7426f4d438a1a249b6fb80240e597be87756fe
Bigcasin <adrianbigcas9900@gmail.com>
2025-12-02 23:38:17 +0800

    Added attributes for ReservationManager, VehicleInspection, and VehicleManager

--- Classes/ReservationManager.cs
+++ Classes/ReservationManager.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace VehicleRENTAL.Classes
 {
+    public enum DateRange
+    {
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly,
+        Custom
+    }
     public class ReservationManager
     {
-    }
+        public Reservation CreateReservation(Customer customer, Vehicle vechicle, DateRange date)
+        {
+            return new Reservation();
+        }
+        public void CancelReservation() 
+        {
+            
+        }
+        public bool CheckAvailability(int reservationid) 
+        {
+            return false;
+        }
+        public string GenerateReservationNumber() 
+        {
+            return new Random().Next(100000, 999999).ToString();
+        }
+    } 
 }
--- Classes/VehicleInspection.cs
+++ Classes/VehicleInspection.cs
@@ -8,5 +8,11 @@ namespace VehicleRENTAL.Classes
 {
     public class VehicleInspection
     {
+        public int InspectionId { get; set; }
+        public Rental Rental { get; set; }
+        public double OdometerReading { get; set; }
+        public double FuelLevel { get; set; }
+        public string InteriorCondition { get; set; }
+        public string ExteriorCondition { get; set; }
     }
 }
