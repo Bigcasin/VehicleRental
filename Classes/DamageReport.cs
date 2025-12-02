e6d7143edd79681ce8927d8a377d354455fcace1
Bigcasin <adrianbigcas9900@gmail.com>
2025-12-02 21:31:58 +0800

    added attributes to some classes again

--- Classes/DamageReport.cs
+++ Classes/DamageReport.cs
@@ -8,5 +8,10 @@ namespace VehicleRENTAL.Classes
 {
     public class DamageReport
     {
+        public int ReportId { get; set; }
+        public Rental Rental { get; set; }
+        public List<Damage> Damages { get; set; }
+        public decimal EstimatedCost { get; set; }
+        public bool ApprovedByAdmin { get; set; }
     }
 }
--- Classes/Rental.cs
+++ Classes/Rental.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace VehicleRENTAL.Classes
 {
+    public enum RentalStatus
+    {
+        Pending,        
+        Active,         
+        Completed,      
+        Cancelled,      
+        Overdue         
+    }
     public class Rental
     {
+        public int RentalId { get; set; }
+        public Reservation Reservation { get; set; }
+        public DateTime PickupTime { get; set; }
+        public DateTime ReturnTime { get; set; }
+        public double FinalMileage { get; set; }
+        public RentalStatus Status { get; set; }
     }
 }
