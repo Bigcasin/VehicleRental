1bbe27755f37fe85150380f14f2e41a411c39222
Bigcasin <adrianbigcas9900@gmail.com>
2025-12-19 23:30:52 +0800

    added Car class but diidint remove the other car classes just in case

---
+++ Models/Cars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleRENTAL
+{
+    public enum Types 
+    {
+        SUV,
+        Sedan,
+        Hatchack,
+        Pickups
+    }
+    public class Cars
+    {
+        public List<Types> CarTypes()
+        {
+            return new List<Types>
+        {
+            Types.SUV,
+            Types.Sedan,
+            Types.Hatchack,
+            Types.Pickups
+        };
+        }
+    }
+}
