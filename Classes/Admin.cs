9426b7816e8964bdc4208a8f4631c1230c30d54a
Bigcasin <adrianbigcas9900@gmail.com>
2026-01-08 20:48:16 +0800

    Updated Admin class

--- Classes/Admin.cs
+++ Classes/Admin.cs
@@ -3,7 +3,7 @@
 
 namespace VehicleRENTAL.Classes
 {
-   
+
     public class Admin : User
     {
         public int AdminId { get; set; }
@@ -19,49 +19,36 @@ namespace VehicleRENTAL.Classes
             Permissions.Add("Reports");
             Permissions.Add("DamageApproval");
         }
-
-namespace VehicleRENTAL.Classes {
-    
-    public class Admin : User {
-        public int adminId;
-        private AdminLevel adminlvl;
-        private List<string> permissions;
-
-
-        // Add a constructor that calls the base User constructor with username and password
         public Admin(string username, string password) : base(username, password)
         {
 
             CustomerManager cm = new CustomerManager();
             cm.LoadCustomers();   // or whatever method you already have
         }
-
-        public void GenerateReports() 
+        public void GenerateReports()
         {
             ReportManager rm = new ReportManager();
             ReportModel report = rm.GenerateAllReports();
 
             // Initialize other fields if needed
             permissions = new List<string>();
+            rm.PrintReport(report);
         }
-
-        public void ManageUsers() {
+        public void ManageUsers()
+        {
         }
 
         // Admin action
-        public void ManageSystem() {
-        // Admin can manage users, vehicles, and reports
+        public void ManageSystem()
+        {
+            // Admin can manage users, vehicles, and reports
         }
-
-        public void GenerateReports() {
-
-
-            rm.PrintReport(report);
-        }
-        public void ConfigureUi() {
+        public void ConfigureUi()
+        {
 
         }
-        public void DamageClaims() {
+        public void DamageClaims()
+        {
 
         }
     }
